39fa7b7b874c7beed5fe8b55bc7ad105f2b5b236
CyrilSharma <sharma.cyril@gmail.com>
2022-03-14 11:22:23 -0400

    cleaner weighting system

--- Navigator.py
+++ Navigator.py
@@ -66,9 +66,8 @@ class SimulatedNavigator():
         redArea = closestBuoys['Red'].width * closestBuoys['Red'].height
         greenx = closestBuoys['Green'].x
         greenArea = closestBuoys['Green'].width * closestBuoys['Green'].height
-        weightDegree = 2.5
-        
-        weight = ((2 * weightDegree - 1) * greenArea + redArea) / ((2 * weightDegree) * (redArea + greenArea))
+        weightDegree = 0.80
+        weight = (weightDegree * greenArea + (1 - weightDegree) * redArea) / (redArea + greenArea)
         avgX = (weight * redx + (1 - weight) * greenx)
         attraction = (avgX - centerx) * 0.1
 
