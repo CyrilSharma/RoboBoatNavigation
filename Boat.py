a6f5f18ea9f851cebf65daedd3929ff4c0891053
pfranktj <44652625+pfranktj@users.noreply.github.com>
2022-03-15 10:14:35 -0400

    work in progress

--- Boat.py
+++ Boat.py
@@ -1,14 +1,16 @@
 import dronekit as dk
 from dronekit import mavutil
+import Constants
+from Navigator import PixhawkNavigator
 # import navigation package
 # import cv
 
 
 class Boat():
     # port name like /dev/ttyAMA0
-    def __init__(self, port_name, id):
-        self.id = id
-        self.vehicle = dk.connect(port_name=None, baud=57600, wait_ready=True)
+    def __init__(self, port_name, name, baudrate):
+        self.id = name
+        self.vehicle = dk.connect(port_name=None, baud=baudrate, wait_ready=True)
         print('Successfully connected')
         self.check_in()
         self.updateState()
@@ -27,9 +29,9 @@ class Boat():
         print(f" Velocity: {self.vehicle.velocity}")
     
     def updateState(self):
-        self.position = self.vehicle.location.local_frame
-        self.velocity = self.vehicle.velocity
-        self.heading = self.vehicle.heading
+        self.position = self.vehicle.location.local_frame  # NED (N=y, E=x)
+        self.velocity = self.vehicle.velocity  # [vx, vy, vz]
+        self.heading = self.vehicle.heading  # [0, 360] where 0 degrees North
 
     # velocity as a list
     # local velocity
@@ -45,20 +47,10 @@ class Boat():
             0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
         self.vehicle.send_mavlink(msg)
 
-    # relative coordinates to beginning position in NED
-    # North (x), East (y), Down (z) (not used)
-    def get_position(self):
-        return self.vehicle.location.local_frame
-    
-    # returns velocity as [vx, vy, vz] in m/s
-    def get_velocity(self):
-        return self.vehicle.velocity
-    
-    # returns current heading in degrees [0, 360] where 0 deg is N
-    def get_heading(self):
-        return self.vehicle.heading
-    
-    
+    def run_navigator(config):
+        # create navigator, pass config
+        # run method, pass closest buoys, 
+        pass
     # def get_buoy_positions
     # from cv get positions relative to frame
 
