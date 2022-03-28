f18054cf4a9c6494d92b49f98163d1fcf8ec8c52
pfranktj <44652625+pfranktj@users.noreply.github.com>
2022-03-28 15:53:08 -0400

    Delete buoy.py

--- buoy.py
+++
@@ -1,8 +0,0 @@
-# define a simple buoy class to contain information about a buoy
-# mostly for compatibility with Navigator class
-class Buoy():
-    def __init__(self, center, area, color):
-        self.x = center[0]
-        self.y = center[1]
-        self.area = area
-        self.color = color
