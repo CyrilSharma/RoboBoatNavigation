3926116d78e8d1a3b272a73938382241f1c4394d
pfranktj <44652625+pfranktj@users.noreply.github.com>
2022-03-27 20:46:22 -0400

    changed buoy class to remove simulation components

--- Buoy.py
+++ Buoy.py
@@ -1,22 +1,8 @@
-import Constants
+# define a simple buoy class to contain information about a buoy
+# mostly for compatibility with Navigator class
 class Buoy():
-    def __init__(self, center, color):
+    def __init__(self, x, y, area, color):
         self.x = center[0]
         self.y = center[1]
-        self.height = Constants.BUOY_HEIGHT
-        self.width = Constants.BUOY_WIDTH
+        self.area = area
         self.color = color
-        self.pixelData = Snapshot()
-    
-    def updatePixelData(self, center, width, height):
-        self.pixelData.x = center[0]
-        self.pixelData.y = center[1]
-        self.pixelData.width = width
-        self.pixelData.height = height
-
-class Snapshot():
-    def __init__(self):
-        self.x = None
-        self.y = None
-        self.width = None
-        self.height = None
