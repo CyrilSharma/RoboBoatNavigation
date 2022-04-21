928a0f09679e8cdae88a9fd3dfa26f6f182d19f0
CyrilSharma <sharma.cyril@gmail.com>
2022-04-21 09:16:36 -0400

    pushed forgotten connection changes

--- dronekit_test.py
+++ dronekit_test.py
@@ -2,11 +2,11 @@
 from dronekit import connect, VehicleMode
 import time
 
-connection_string = "/dev/ttyAMA0"
+connection_string = "/dev/serial0"
 
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+vehicle = connect(connection_string, wait_ready=True, baud=960000)
 
 
 # Get some vehicle attributes (state)
